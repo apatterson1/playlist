7327f6fc3686f50510b2e64f84890dc668e2a1f9
apatterson1 <apatterson@hs-gc.org>
2018-03-15 22:19:14 +0000

    Creating for loop to display playlist + editing songs

--- js/script.js
+++ js/script.js
@@ -17,49 +17,60 @@
 
 // Songs
 var mySong = {
-	"title":"24K Magic",
-	"artist":"Bruno Mars",
-	"mp3-url":"https://open.spotify.com/track/6b8Be6ljOzmkOmFslEb23P",
-	"image-url":"https://images-na.ssl-images-amazon.com/images/I/71Gr9aCHQfL._SY355_.jpg",
+	"title":"Some",
+	"artist":"Steve Lacy",
+	"mp3Url":"https://open.spotify.com/album/4POiurFeB5bDsF9yBwOxbh",
+	"imageUrl":"https://t2.genius.com/unsafe/220x0/https%3A%2F%2Fimages.genius.com%2Fcfcfadff10b7429caf30257cfa3b00f3.500x500x1.jpg",
+	"songRating": "4/5"
 }
 
 var myPlayList = [
 	{
-		"title":"24K Magic",
-		"artist":"Bruno Mars",
-		"mp3-url":"https://open.spotify.com/track/6b8Be6ljOzmkOmFslEb23P",
-		"image-url":"https://images-na.ssl-images-amazon.com/images/I/71Gr9aCHQfL._SY355_.jpg",
+		"title":"Skyline To",
+		"artist":"Frank Ocean",
+		"mp3Url":"https://open.spotify.com/track/4xR3MAscflQ262kMeiKshQ",
+		"imageUrl":"https://t2.genius.com/unsafe/220x0/https%3A%2F%2Fimages.genius.com%2Fccc0894094b9a3cbb47a45ec0c69b1f0.1000x1000x1.jpg",
 	},
 	{
-		"title":"Sir Duke",
-		"artist":"Stevie Wonder",
-		"mp3-url":"https://open.spotify.com/track/2udw7RDkldLFIPG9WYdVtT",
-		"image-url":"https://upload.wikimedia.org/wikipedia/en/thumb/e/e2/Songs_in_the_key_of_life.jpg/220px-Songs_in_the_key_of_life.jpg",
+		"title":"Some",
+	"artist":"Steve Lacy",
+	"mp3Url":"https://open.spotify.com/album/4POiurFeB5bDsF9yBwOxbh",
+	"imageUrl":"https://t2.genius.com/unsafe/220x0/https%3A%2F%2Fimages.genius.com%2Fcfcfadff10b7429caf30257cfa3b00f3.500x500x1.jpg",
 	},
 	{
-		"title":"Sorry",
-		"artist":"Justin Bieber",
-		"mp3-url":"https://open.spotify.com/track/09CtPGIpYB4BrO8qb1RGsF",
-		"image-url":"http://assets-s3.usmagazine.com/uploads/assets/articles/93827-justin-biebers-sorry-choreographer-spills-video-style-secrets-parris-goebel/1445638548_justin-bieber-sorry-dancers-zoom.jpg",
+		"title":"BabyWipe",
+		"artist":"Ski Mask the Slump God",
+		"mp3Url":"https://open.spotify.com/album/2wj14855mFHE9XCP5GNWnU",
+		"imageUrl":"https://t2.genius.com/unsafe/220x0/https%3A%2F%2Fimages.genius.com%2F880edce09def57ac4f54c6dcbc0ac101.500x500x1.jpg",
 	}
 
-]
+];
 
-
+function link_title(title, url) {
+  	return "<a href='" + url + "'>" + title + "</a>";
+  }
 
 // DOCUMENT READY FUNCTION
 $( document ).ready(function() {
   
-
-
+  
+  //$('body').append("<h3>" + link_title(mySong.title, mySong.mp3Url) + "</h3>" + "<p> <img src ='" 
+		// 			+ mySong.imageUrl + "'> </p>" + "<p>" 
+		// 			+ mySong.artist + "</p>" + mySong.songRating);
+displayList();
 
 });
 
 function displayList(){
 
-
-  
+for (var i= 0; i < myPlayList.length; i = i + 1 ) {
+	$('.songs').append("<div>" + "<h3>" + link_title(myPlayList[i].title,myPlayList[i].mp3Url)
+	+ "</h3>" + "<img src ='" + myPlayList[i].imageUrl +"'>"
+	+ "<p>" + myPlayList[i].artist + "</p>");
 }
+
+}
+
 
 function clearList(){
   
